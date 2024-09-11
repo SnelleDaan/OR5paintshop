e5dd42e2cc7315703fdcac22051483cc3812088e
D.Derks <512201@student.fontys.nl>
2024-09-11 12:39:00 +0200

    everything is set up

--- painttest.py
+++ painttest.py
@@ -1,1 +1,28 @@
 colors = ['red','yellow','blue']
+
+kleurwissels = [
+    ("red", "yellow", 2),
+    ("yellow", "blue", 3),
+    ("blue", "red", 1),
+]
+setup_times = {}
+for prev_color, current_color, interval in kleurwissels:
+    color_pair = (prev_color, current_color)
+    color_pair2 = (current_color, prev_color)
+    if color_pair in setup_times:
+        setup_times[color_pair].append(interval)
+        setup_times[color_pair2].append(interval)
+    else:
+        setup_times[color_pair] = [interval]
+        setup_times[color_pair2] = [interval]
+
+# Order, Surface, Color, Deadline, Penalty
+orders = [(1, 10, 'yellow', 18, 10), (2, 5, 'red', 10, 2), (3, 15, 'blue', 20, 8)]
+
+machines = ['M1','M2']
+
+machine_speed = {
+    'M1': 10,
+    'M2': 15
+}
+
