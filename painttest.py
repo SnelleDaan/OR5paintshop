68b5c0182eadef1e59a5730a0124ade624432fd7
D.Derks <512201@student.fontys.nl>
2024-10-07 18:28:04 +0200

    schedule gemaakt van voorbeeld

--- painttest.py
+++ painttest.py
@@ -17,11 +17,17 @@ for prev_color, current_color, interval in kleurwissels:
         setup_times[color_pair].append(interval)
         setup_times[color_pair2].append(interval)
     else:
-        setup_times[color_pair] = [interval]
-        setup_times[color_pair2] = [interval]
+        setup_times[color_pair] = interval
+        setup_times[color_pair2] = interval
 
-# Order, Surface, Color, Deadline, Penalty
-orders = [(1, 10, 'yellow', 18, 10), (2, 5, 'red', 10, 2), (3, 15, 'blue', 20, 8)]
+
+# Order, area, Color, Deadline, Penalty
+orders = [
+    {'area': 100, 'color': 'red'},   # Order 1: 100 m² of red paint
+    {'area': 150, 'color': 'blue'},  # Order 2: 150 m² of blue paint
+    {'area': 80, 'color': 'red'}     # Order 3: 80 m² of red paint
+]
+
 
 machines = {
     'Machine1': {'speed': 10},  # m²/hour
@@ -29,39 +35,49 @@ machines = {
     'Machine3': {'speed': 20}
 }
 
-'''
-calculate time of all orders with one machine
-'''
-#list of completed orders
-x1 = []
-ordercompleted = []
-totaltime = 0
-changetime = 0
-oldcolor = ''
-for order, surface, color, deadline, penalty in orders:
-    ordertime = surface/machine_speed['M1']
-    x1.append(ordertime)
-    ordercompleted.append(order)
-    if color != oldcolor and oldcolor != '':
-        changetime = setup_times[oldcolor,color][0]
-    else:
-        changetime = 0
-    totaltime = totaltime + ordertime + changetime
-    oldcolor = color
 
-def painttime(area, machine):
-    return area/ machine['speed']
+def painttime(area, machine, machines):
+    for machine_name, machine_data in machines.items():
+        if machine == machine_name:
+            speed = machine_data['speed']
+    return area/ speed
 
-def switchtime(order, machine, current_color):
-    if current_color != order['color']:
-        return 
-    else:
+# Calculate the color switch time between previous and current color
+def switchtime(prev_color, current_color):
+    if prev_color is None:  # No switch time if the machine has not painted yet
         return 0
+    if (prev_color, current_color) in setup_times:
+        return setup_times[(prev_color, current_color)]
+    return 0
 '''
 visualisize the planning
 '''
-import matplotlib.pyplot as plt     # for visualization of the tours
-y1 = []
-for x in x1:
-    y1.append(1)
-plt.scatter(x1,y1)
+
+
+def schedule_orders(orders, machines):
+    schedule = []
+    machine_states = {machine: {'current_color': None, 'available_time': 0} for machine in machines}
+    
+    for order in orders:
+        best_machine = None
+        best_time = float('inf')
+        
+        for machine_name, machine_data in machines.items():
+            current_machine = machine_states[machine_name]
+            switch_time = switchtime(current_machine['current_color'], order['color']) 
+            paint_time = painttime(order['area'], machine_name, machines)
+            time_to_complete = switch_time + paint_time
+            completion_time = current_machine['available_time'] + time_to_complete
+            
+            if completion_time < best_time:
+                best_time = completion_time
+                best_machine = machine_name
+        
+        # Update the chosen machine state
+        schedule.append({'order': order, 'machine': best_machine, 'start_time': machine_states[best_machine]['available_time']})
+        machine_states[best_machine]['available_time'] = best_time
+        machine_states[best_machine]['current_color'] = order['color']
+    
+    return schedule
+
+print(schedule_orders(orders, machines))
